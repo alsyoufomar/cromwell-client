1ffffad15e2083d58039f3b8c68238af91ef01e3
Omar Alsyouf <omaralsoyouf@gmail.com>
2022-09-19 22:39:25 +0100

    build landing and home pages

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,42 @@
+import '../styles/home.css';
+
 const Home = () => {
-  return <div className='home'>Home</div>;
+  return (
+    <div className='home'>
+      <div className='home__hero'>
+        <h1>Welcome, Omar!</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde culpa
+          accusantium quidem. Quod numquam voluptas quas labore soluta.
+          Veritatis, assumenda. Eaque eum aut molestias eos aliquid est natus
+          obcaecati quod?
+        </p>
+      </div>
+      <div className='home__fake-content'>
+        <h2>What our partners have to say:</h2>
+        <div className='home__fake-content__details'>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde culpa
+            accusantium quidem. Quod numquam voluptas quas labore soluta.
+            Veritatis, assumenda. Eaque eum aut molestias eos aliquid est natus
+            obcaecati quod?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, officia
+            suscipit, obcaecati, vero a amet sit commodi voluptatem aut et neque
+            laudantium ex blanditiis ipsum sapiente architecto quasi eos
+            tenetur.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
+            laboriosam blanditiis illo error, quibusdam labore est corrupti
+            consectetur laudantium, officia eum! Harum aperiam cumque vitae iure
+            nisi qui similique aspernatur!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,5 +1,48 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/landing.css';
+
 const Landing = () => {
-  return <div className='landing'>Landing</div>;
+  const [userData, setUserData] = useState({});
+  const host = process.env.REACT_APP_API_URL;
+
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + localStorage.getItem('jwt'),
+      },
+    };
+    fetch(`${host}/user/info`, options)
+      .then((res) => {
+        if (!res.ok && res.error)
+          throw Error('could not fetch the data from the source');
+        return res.json();
+      })
+      .then((res) => {
+        setUserData(res.user);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (!userData) return <></>;
+  return (
+    <div className='landing'>
+      <div className='landing__hero-container'>
+        <h1>Here is your Looovely details!</h1>
+        <div className='landing__details'>
+          <h2>
+            {userData.firstname} {userData.lastname}
+          </h2>
+          <h2>{userData.email}</h2>
+        </div>
+        <Link to='/home'>
+          <button className='landing__btn'>Continue to Home!</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Landing;
