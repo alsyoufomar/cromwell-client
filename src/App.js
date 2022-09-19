d1c6a0297c3982f3f80ed2327d50a00347c74658
Omar Alsyouf <omaralsoyouf@gmail.com>
2022-09-19 19:51:50 +0100

    Routing functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
 import Register from './components/Register';
 import Landing from './components/Landing';
-import Navigation from './components/Nav';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className='App'>
-      <Home />
+      <Nav />
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/landing' element={<Landing />} />
+      </Routes>
     </div>
   );
 }
